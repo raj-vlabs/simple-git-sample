9f8421818b73c6c0a5182c5f67ec3069e6e94005
r <raj@vlabs.ac.in>
2023-04-14 19:11:42 +0530

    uploaded more files

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const repoUrl = 'https://github.com/raj-vlabs/simple-git-sample.git'
 try {
 	git.add('./*')
 		.commit('uploaded files')
+		.addRemote('origin', repoUrl);
    await git.push(repoUrl, 'main');
     console.log("push successful")
 } catch (e) {
     console.log("Error::", e)
 }
+// Dummy comment for pushing
